a06d38de18089c6bbb81f3155a996a366357b709
Rafiqdevhub <rafiq85825@gmail.com>
2025-09-25 21:56:34 +0500

    Backend url added

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -5,8 +5,7 @@ import type {
   InternalAxiosRequestConfig,
 } from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 interface ExtendedAxiosRequestConfig extends InternalAxiosRequestConfig {
   _retry?: boolean;
